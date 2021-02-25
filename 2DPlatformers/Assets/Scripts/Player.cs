a93936c8c2f244de9ec9d6db0f2e8e7ad92d0194
Cooper Grimes <grimescb@hendrix.edu>
2021-02-24 19:05:00 -0600

    Demo Level
    The player can move horizontally, jump once, and navigate across medium and small sized platform prefabs. The jumping does not look so smooth and once the player gets high enough they shoot upwards.

---
+++ 2DPlatformers/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D body;
+
+    private float horizontal;
+    public float runSpeed = 10f;
+    public float jumpForce = 400f;
+    private bool jumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown("space") && !jumping)
+        {
+            body.AddForce(transform.up * jumpForce);
+            jumping = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(horizontal * runSpeed, body.position.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumping = false;
+    }
+}
