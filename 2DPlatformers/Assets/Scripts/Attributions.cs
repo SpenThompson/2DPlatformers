6d0556442ff0214328b59c28addf191c9dc1a348
Spencer <james.spencer.thompson@gmail.com>
2021-02-25 18:16:34 -0600

    Attributions Added in Script

---
+++ 2DPlatformers/Assets/Scripts/Attributions.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attributions : MonoBehaviour
+{
+    // Background - Ansimuz https://opengameart.org/content/space-background-3
+    // Buttons - AhmadManga https://opengameart.org/content/semi-transparent-buttons
+    // Font - Grafito Design https://www.fontspace.com/lcd-phone-font-f11711
+    // Sprite - 
+    // Music - Goose Ninja https://gooseninja.itch.io/space-music-pack
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
